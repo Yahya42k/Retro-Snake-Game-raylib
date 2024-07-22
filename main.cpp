f897a44d7179a5da13cec7f3384c468eb9b3f321
yahya42k <yahya.abdurrahim@gmail.com>
2024-07-22 19:20:10 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -3,12 +3,49 @@
 
 using namespace std;
 
+Color green={173,204,96,255};
+Color darkGreen={43,51,24,255};
+
+int cellSize=30;
+int cellCount=25;
+
+
+class Food{
+public:
+Vector2 position={5,6};
+
+void Draw(){
+    DrawRectangle(position.x*cellSize,position.y*cellSize,cellSize,cellSize,darkGreen);
+}
+
+};
+
 int main () {
 
    
 
-    cout << "Hello World" << endl;
-
+ InitWindow(cellSize*cellCount,cellSize*cellCount,"Retro Snake");
+SetTargetFPS(60);
    
+Food food=Food();
+
+
+while(WindowShouldClose()==false){
+
+    BeginDrawing();
+
+//drawing
+ClearBackground(green);
+food.Draw();
+
+    EndDrawing();
+
+}
+
+
+
+
+
+   CloseWindow();
     return 0;
 }
